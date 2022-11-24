308c63d48e8f6340809d0cdd5a04e7bc8405c3bf
Kapshai <leonid.kapshai@gmail.com>
2022-11-24 23:48:30 +0300

    3 задача

---
+++ z-23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Console.WriteLine("Введите число:");
+// var num = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 1; i <= num; i=++)
+// {
+//     Console.WriteLine(Math.Pow(i,3));
+// }
+Console.WriteLine("Введите число:");
+var num = Convert.ToInt32(Console.ReadLine());
+int num2 = 1;
+
+while (num2 <= num)
+{
+    Console.WriteLine(num2*num2*num2);
+    num2++;
+}
