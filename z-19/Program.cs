d720ad26261b6530b148a06bb1fe2a563738e4b9
Kapshai <leonid.kapshai@gmail.com>
2022-11-24 23:18:46 +0300

    Первая задача

---
+++ z-19/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите пятизначное число:");
+var number = Convert.ToInt32(Console.ReadLine());
+if (number > 9999 && number < 100000) 
+{
+    var sum = 0;    
+    sum = (number%10)*10000;
+    sum = ((number%100-number%10)*100)+sum;
+    sum = (number%1000-number%100)+sum;
+    sum = ((number%10000-number%1000)/100)+sum;
+    sum = ((number%100000-number%10000)/10000)+sum;
+    if(sum == number)//
+    {
+        Console.WriteLine("Число полиндром");
+    }
+    else
+    {
+    Console.WriteLine("Число не полиндром");
+    }
+}
+else
+    {
+    Console.WriteLine("Число не пятизначное");
+    }
